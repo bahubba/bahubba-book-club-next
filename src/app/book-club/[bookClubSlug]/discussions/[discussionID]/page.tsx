a12728b9276ba8d0106528cf762c725ed5e7c4ca
bahubba89@gmail.com <bahubba89@gmail.com>
2024-06-21 18:16:09 -0400

    BAHubba-CU-86azbrjtf: Discussion Replies
    Finis
    
    Reformatted and simplified, reorganized suspenses

--- src/app/book-club/[bookClubSlug]/discussions/[discussionID]/page.tsx
+++ src/app/book-club/[bookClubSlug]/discussions/[discussionID]/page.tsx
@@ -1,22 +1,15 @@
 import { Suspense } from 'react';
 
 import { getDiscussion } from '@/api/fetchers/discussion.fetchers';
-import ReplyButton from '@/components/buttons/reply.button';
 import DiscussionReplyList from '@/components/lists/discussion-reply.list';
+import { Spinner } from '@nextui-org/spinner';
 
 // Component props
-interface Slugs {
-  bookClubSlug: string;
-  discussionID: string;
-}
-
-interface ReplyListProps extends Slugs {
-  pageNum: number;
-  pageSize: number;
-}
-
 interface BookClubDiscussionPageProps {
-  params: Slugs;
+  params: {
+    bookClubSlug: string;
+    discussionID: string;
+  };
   searchParams?: {
     pageNum?: string;
     pageSize?: string;
@@ -24,20 +17,22 @@ interface BookClubDiscussionPageProps {
 }
 
 /**
- * Async component for the book club discussion header
+ * Book club discussion page
  *
- * @param {Object} props Component props
- * @param {string} props.bookClubSlug The slug of the book club
- * @param {string} props.discussionID The slug of the discussion
- * @param {number} props.pageNum The page number from the URL query
- * @param {number} props.pageSize The page size from the URL query
+ * @param {Object} props - Component props
+ * @param {Object} props.params - The route parameters
+ * @param {string} props.params.bookClubSlug - The slug of the book club
  */
-const BookClubDiscussionContent = async ({
-  bookClubSlug,
-  discussionID,
-  pageNum,
-  pageSize
-}: Readonly<ReplyListProps>) => {
+const BookClubDiscussionPage = async ({
+  params: { bookClubSlug, discussionID },
+  searchParams: {
+    pageNum = '1',
+    pageSize = '10'
+  } = {
+    pageNum: '1',
+    pageSize: '10'
+  }
+}: Readonly<BookClubDiscussionPageProps>) => {
   // Load the discussion
   const discussion = await getDiscussion(bookClubSlug, discussionID);
 
@@ -48,44 +43,23 @@ const BookClubDiscussionContent = async ({
         <p className="text-gray-500">{discussion.description}</p>
       </div>
       <div className="flex flex-1 flex-col w-full h-1">
-        <Suspense fallback={<></>}>
+        <Suspense
+          fallback={
+            <div className="flex justify-center items-center w-full h-full">
+              <Spinner/>
+            </div>
+          }
+        >
           <DiscussionReplyList
             bookClubSlug={bookClubSlug}
             discussionID={discussionID}
-            pageNum={pageNum}
-            pageSize={pageSize}
+            pageNum={parseInt(pageNum)}
+            pageSize={parseInt(pageSize)}
           />
         </Suspense>
       </div>
     </>
   );
 };
-
-/**
- * Book club discussion page
- *
- * @param {Object} props - Component props
- * @param {Object} props.params - The route parameters
- * @param {string} props.params.bookClubSlug - The slug of the book club
- */
-const BookClubDiscussionPage = ({
-  params: { bookClubSlug, discussionID },
-  searchParams: {
-    pageNum = '1',
-    pageSize = '10'
-  } = {
-    pageNum: '1',
-    pageSize: '10'
-  }
-}: Readonly<BookClubDiscussionPageProps>) => (
-  <Suspense fallback={<></>}>
-    <BookClubDiscussionContent
-      bookClubSlug={bookClubSlug}
-      discussionID={discussionID}
-      pageNum={parseInt(pageNum)}
-      pageSize={parseInt(pageSize)}
-    />
-  </Suspense>
-);
 
 export default BookClubDiscussionPage;
--- src/components/lists/discussion-reply.list.tsx
+++ src/components/lists/discussion-reply.list.tsx
@@ -1,13 +1,12 @@
 import { Fragment, Suspense } from 'react';
-import { redirect } from 'next/navigation';
 import { Divider } from '@nextui-org/divider';
 import { User } from '@nextui-org/user';
-
-import { getDiscussionReplies, getDiscussionReplyCount } from '@/api/fetchers/discussion.fetchers';
-import { Pagination } from '@nextui-org/pagination';
+import { ScrollShadow } from '@nextui-org/scroll-shadow';
+import { Spinner } from '@nextui-org/spinner';
 import URLQueryPagination from '@/components/pagination/url-query.pagination';
 import ReplyButton from '@/components/buttons/reply.button';
-import { ScrollShadow } from '@nextui-org/scroll-shadow';
+
+import { getDiscussionReplies, getDiscussionReplyCount } from '@/api/fetchers/discussion.fetchers';
 
 // Component props
 interface DiscussionReplyListProps {
@@ -17,60 +16,8 @@ interface DiscussionReplyListProps {
   pageNum: number;
 }
 
-interface DiscussionRepliesProps {
-  bookClubSlug: string;
-  discussionID: string;
-  pageNum?: number;
-  pageSize?: number;
-}
-
 /**
- * Async component for loading paginated replies
- *
- * @param {Object} props Component props
- * @param {string} props.bookClubSlug The slug of the book club
- * @param {string} props.discussionID The ID of the discussion
- * @param {number} props.pageNum The page number
- * @param {number} props.pageSize The number of replies per page
- */
-const DiscussionReplies = async ({
-  bookClubSlug,
-  discussionID,
-  pageNum = 1,
-  pageSize = 10
-}: DiscussionRepliesProps) => {
-  // Fetch the replies
-  const replies = await getDiscussionReplies(bookClubSlug, discussionID, pageSize, pageNum - 1);
-
-  return (
-    <ScrollShadow
-      hideScrollBar
-      className="flex-1 h-1 overflow-auto"
-    >
-      {
-        replies.map((reply, idx) => (
-          <Fragment key={reply.id}>
-            <div className="flex-col w-full space-y-1">
-              <div className="text-medium pt-2">{ reply.content }</div>
-              <User
-                name={reply.user.preferredName}
-                avatarProps={{
-                  src: reply.user.preferredImage || undefined,
-                  alt: `${reply.user.preferredName || 'user'} avatar`,
-                  size: 'sm'
-                }}
-              />
-            </div>
-            {idx < replies.length - 1 && <Divider />}
-          </Fragment>
-        ))
-      }
-    </ScrollShadow>
-  )
-}
-
-/**
- * Paginated display of discussion replies
+ * Async component for paginated display of discussion replies
  *
  * @param {Object} props Component props
  * @param {string} props.bookClubSlug The slug of the book club
@@ -86,31 +33,57 @@ const DiscussionReplyList = async ({
   // Get the total number of replies
   const total = await getDiscussionReplyCount(bookClubSlug, discussionID);
 
+  // Fetch the replies
+  const replies = await getDiscussionReplies(bookClubSlug, discussionID, pageSize, pageNum - 1);
+
   return (
     <div className="flex flex-col h-full">
-      <Suspense fallback={<></>}>
-        <DiscussionReplies
-          bookClubSlug={bookClubSlug}
-          discussionID={discussionID}
-          pageNum={pageNum}
-          pageSize={pageSize}
-        />
+      <Suspense
+        fallback={
+          <div className="flex-1 flex h-1 justify-center items-center">
+            <Spinner />
+          </div>
+        }
+      >
+        <ScrollShadow
+          hideScrollBar
+          className="flex-1 h-1 overflow-auto"
+        >
+          {
+            replies.map((reply, idx) => (
+              <Fragment key={ reply.id }>
+                <div className="flex-col w-full space-y-1">
+                  <div className="text-medium pt-2">{ reply.content }</div>
+                  <User
+                    name={ reply.user.preferredName }
+                    avatarProps={ {
+                      src: reply.user.preferredImage || undefined,
+                      alt: `${ reply.user.preferredName || 'user' } avatar`,
+                      size: 'sm'
+                    } }
+                  />
+                </div>
+                { idx < replies.length - 1 && <Divider/> }
+              </Fragment>
+            ))
+          }
+          <div className="flex justify-start">
+            <ReplyButton
+              bookClubSlug={ bookClubSlug }
+              discussionID={ discussionID }
+              nodeID={ discussionID }
+              replyToText={ 'Reply' }
+              rootReply
+            />
+          </div>
+        </ScrollShadow>
+        <div className="flex justify-center">
+          <URLQueryPagination
+            url={`/book-club/${bookClubSlug}/discussions/${discussionID}`}
+            total={Math.ceil(total / pageSize)}
+          />
+        </div>
       </Suspense>
-      <div className="flex justify-start">
-        <ReplyButton
-          bookClubSlug={bookClubSlug}
-          discussionID={discussionID}
-          nodeID={discussionID}
-          replyToText={'Reply'}
-          rootReply
-        />
-      </div>
-      <div className="flex justify-center">
-        <URLQueryPagination
-          url={`/book-club/${bookClubSlug}/discussions/${discussionID}`}
-          total={Math.ceil(total / pageSize)}
-        />
-      </div>
     </div>
   )
 }
